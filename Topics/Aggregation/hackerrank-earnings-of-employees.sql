1e627e89ad4af7d34bf05b050a28d4479ed88e87
Caio Henrique <caio.hda@gmail.com>
2022-09-03 09:07:45 -0300

    Add aggregation and basic-join exercises

---
+++ Topics/Aggregation/hackerrank-earnings-of-employees.sql
@@ -0,0 +1,4 @@
+
+select max(months * salary), count(employee_id)
+from Employee
+where (months * salary) = (select max(months * salary) from employee);
---
+++ Topics/Basic-Join/hackerrank-population-census.sql
@@ -0,0 +1,6 @@
+
+select sum(CITY.POPULATION)
+from CITY
+inner join COUNTRY
+on (CITY.COUNTRYCODE = COUNTRY.CODE)
+where CONTINENT  = 'Asia';
